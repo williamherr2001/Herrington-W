d1a679caa5e4921ce391582737fd2cd7d8f1b7af
williamherr2001 <williamherr2001@gmail.com>
2018-10-04 18:11:28 -0400

    Create a.java

---
+++ Codeforces/Educational Round 50/a.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class a {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		long n = in.nextLong(), k = in.nextLong();
+		System.out.println(((k+n-1)/n));
+
+	}
+
+}

d7bc9a14e873da83552272ccb485c121209a57ec
williamherr2001 <williamherr2001@gmail.com>
2018-10-04 08:55:35 -0400

    Codeforces Round 506 - A
    Problem A - Many Equal Substrings from Codeforces Round #506.
    I solved this problem by starting at index 1 of a given string. I then took a substring from the current index to the end of the string, checking to see if the given string started also with the substring. If so, the program would then record the current index as an integer and break the loop. It then would take the original string and append the substring found above k-1 times.

--- a.java
+++ a.java
@@ -4,9 +4,23 @@ public class a {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		
 		int n = in.nextInt();
-		int s = Integer.toBinaryString(n).length();
-		System.out.println(s);
+		for(int i = 0; i<n; i++) {
+			int m = in.nextInt();
+			String s = in.next();
+			char[] c = s.toCharArray();
+			boolean tf = true;
+			for(int j = 0, k = m-1;j<k;j++,k--) {
+				if(Math.abs((c[j]+"").compareTo(c[k]+""))>2 ||Math.abs((c[j]+"").compareTo(c[k]+""))==1) {
+					tf = false; 
+					break;
+				}
+			}
+			if(tf)System.out.println("YES");
+			else System.out.println("NO");
+		}
+
 	}
 
 }
